445640e279d8e41216d0e2f149f83bf2c8287cbf
boolfalse <extravaganza1024@gmail.com>
2025-05-22 15:49:13 -0700

    React.js + Vite initial integration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Board from "./components/Board";
+
+function App() {
+
+    return (
+        <div>
+            <Board />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Board.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+function Board() {
+
+    return (
+        <>
+            <h2 className="chess-board">Board</h2>
+        </>
+    );
+}
+
+export default Board;
