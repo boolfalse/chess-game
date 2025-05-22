f514a124e9baa5d7cfe1cd0830541b84b604aa1c
boolfalse <extravaganza1024@gmail.com>
2025-05-22 16:56:24 -0700

    Select the piece

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,8 +3,36 @@ import React, {useEffect, useState} from 'react';
 
 
 function Board() {
+    // WHITE is blue, BLACK is red // 1984 :)
+
     const rows = 8, columns = 8;
     const [board, setBoard] = useState([]);
+    const [turn, setTurn] = useState('white');
+    const [lastSelected, setLastCSelected] = useState({ r: -1, c: -1 });
+
+    const pieceSelected = (r: number, c: number) => {
+        // get the clicked cell
+        const cell = document.querySelector(`.cell[data-r="${r}"][data-c="${c}"]`);
+        if (!cell) return;
+
+        const currentOwner = cell.getAttribute('data-current-owner');
+        // owner 1 is white, owner 2 is black
+        if (currentOwner === '' || currentOwner !== turn) {
+            // alert('Invalid move');
+            return;
+        }
+
+        // change the color of the cell
+        if (cell.classList.contains('selected')) {
+            cell.classList.remove('selected');
+            setLastCSelected({ r: -1, c: -1 });
+        } else {
+            const lastCell = document.querySelector(`.cell[data-r="${lastSelected.r}"][data-c="${lastSelected.c}"]`);
+            if (lastCell) lastCell.classList.remove('selected');
+            cell.classList.add('selected');
+            setLastCSelected({ r, c });
+        }
+    }
 
     const initializeBoard = () => {
         const pieces = [
@@ -42,7 +70,12 @@ function Board() {
                 {board.map((row: Array<string>, i: number) => (
                     <div key={i} className="row">
                         {row.map((cell: string, j: number) => (
-                            <div key={j} className={`cell ${((i + j) % 2 === 0) ? 'cell-white' : 'cell-black'}`}>
+                            <div key={j}
+                                 data-current-owner={!cell ? '' : (cell[1] === '1' ? 'white' : 'black')}
+                                 data-r={i}
+                                 data-c={j}
+                                 onClick={() => pieceSelected(i, j)}
+                                 className={`cell ${((i + j) % 2 === 0) ? 'cell-white' : 'cell-black'}`}>
                                 <span className={`piece-${cell}`}>
                                     {cell}
                                 </span>
